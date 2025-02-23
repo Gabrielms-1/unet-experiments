7d7cfd1ad2dd07f77b151308da57bf3b26e93fd8
sagemaker <sagemaker@huggingface.co>
2025-02-23 03:50:11 -0300

    fix custom dataset folder structure

--- src/unet.py
+++ src/unet.py
@@ -30,9 +30,8 @@ class UNet(nn.Module):
         self.upconv1 = nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=2, stride=2)
         self.deconv1 = self.conv_block(in_channels=128, out_channels=64)
 
-        self.final_conv = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=n_classes, kernel_size=1),
-        ) 
+        self.final_conv = nn.Conv2d(in_channels=64, out_channels=n_classes, kernel_size=1)
+
 
     def conv_block(self, in_channels, out_channels):
         """Bloco de convolução: 2 convoluções + BatchNorm + ReLU"""
@@ -58,39 +57,31 @@ class UNet(nn.Module):
         print(f"Shape da saída do encoder 4 (e4): {e4.shape}")
 
         # Bottleneck
-        bottleneck = self.bottleneck(e4)
+        bottleneck = self.bottleneck(F.max_pool2d(e4, kernel_size=2))
         print(f"Shape da saída do bottleneck: {bottleneck.shape}")
         # Decoder
         d4 = self.upconv4(bottleneck)
         print(f"Shape da saída do upconv4 (d4): {d4.shape}")
         d4 = torch.cat((e4, d4), dim=1)
         print(f"Shape da saída do cat (d4): {d4.shape}")
-        d4 = self.conv_block(d4)
-        print(f"Shape da saída do conv_block (d4): {d4.shape}")
         d4 = self.deconv4(d4)
         print(f"Shape da saída do deconv4 (d4): {d4.shape}")
 
         d3 = self.upconv3(d4)
         d3 = torch.cat((e3, d3), dim=1)
         print(f"Shape da saída do cat (d3): {d3.shape}")
-        d3 = self.conv_block(d3)
-        print(f"Shape da saída do conv_block (d3): {d3.shape}")
         d3 = self.deconv3(d3)
         print(f"Shape da saída do deconv3 (d3): {d3.shape}")
 
         d2 = self.upconv2(d3)
         d2 = torch.cat((e2, d2), dim=1)
         print(f"Shape da saída do cat (d2): {d2.shape}")
-        d2 = self.conv_block(d2)
-        print(f"Shape da saída do conv_block (d2): {d2.shape}")
         d2 = self.deconv2(d2)
         print(f"Shape da saída do deconv2 (d2): {d2.shape}")
 
         d1 = self.upconv1(d2)
         d1 = torch.cat((e1, d1), dim=1)
         print(f"Shape da saída do cat (d1): {d1.shape}")
-        d1 = self.conv_block(d1)
-        print(f"Shape da saída do conv_block (d1): {d1.shape}")
         d1 = self.deconv1(d1)
         print(f"Shape da saída do deconv1 (d1): {d1.shape}")
 
